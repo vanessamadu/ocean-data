4971d5c57fc919d61e772ff13b3f18f4ab5cbacf
Vanessa Madu <vm2218@ic.ac.uk>
2024-06-13 15:25:11 +0100

    load metadata function

--- update_script.py
+++ update_script.py
@@ -1,2 +1,11 @@
-import pandas as pd
+from json_functions import read_json_to_dataframe, update_json_file
+import os
 
+def load_metadata(filename):
+    root_path = os.path.dirname(__file__)
+    file_path = os.path.join(root_path, 'metadata\\%s'%filename)
+
+    with open(file_path, 'r') as file:
+        return(file.read()) 
+
+print(load_metadata("data.py"))
