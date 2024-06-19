513975cbef92cac315191b9ae4bee89285ed4b2b
Vanessa Madu <vm2218@ic.ac.uk>
2024-06-19 17:34:33 +0100

    updates

--- update_script.py
+++ update_script.py
@@ -1,6 +1,7 @@
 from json_functions import read_json_to_dataframe, update_json_file
 from data_module_list import *
 
-data = metadata.data1.dataset
 filename = "ocean_data.json"
-update_json_file(filename,data)
+for dataname in data_names:
+    data = dataname.dataset
+    update_json_file(filename,data)
