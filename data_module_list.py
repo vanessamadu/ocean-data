7b6236a35c93b638c3cf410b1824a7667f4f7e46
Vanessa Madu <vm2218@ic.ac.uk>
2024-06-23 12:08:26 +0100

    update

--- data_module_list.py
+++ data_module_list.py
@@ -1,1 +1,25 @@
-import metadata.data1
+import metadata.data1, metadata.data2,metadata.data3,metadata.data4,metadata.data5
+import metadata.data6,metadata.data7,metadata.data8,metadata.data9,metadata.data10
+import metadata.data11,metadata.data12,metadata.data13,metadata.data14, metadata.data15
+import metadata.data16
+
+data_names = [metadata.data1,metadata.data2,metadata.data3,metadata.data4,metadata.data5,metadata.data6,metadata.data7,
+              metadata.data8,metadata.data9,metadata.data10,metadata.data11,metadata.data12,metadata.data13,metadata.data14,
+              metadata.data15,metadata.data16]
+
+dataset_names = ["Global Ocean Gridded L4 Sea Surface Heights And Derived Variables Reprocessed 1993 Ongoing",
+                 "ODYSSEA Global Sea Surface Temperature Gridded Level 4 Daily Multi-Sensor Observations",
+                 "Global Ocean OSTIA Sea Surface Temperature and Sea Ice Analysis",
+                 "ESA SST CCI and C3S reprocessed sea surface temperature analyses",
+                 "Global Ocean OSTIA Sea Surface Temperature and Sea Ice Reprocessed",
+                 "European Seas Gridded L4 Sea Surface Heights and Derived Variables Reprocessed 1993 Ongoing",
+                 "Global Ocean Hourly Sea Surface Wind and Stress from Scatterometer and Model",
+                 "Global Ocean Hourly Reprocessed Sea Surface Wind and Stress from Scatterometer and Model",
+                 "Global Ocean Monthly Mean Sea Surface Wind and Stress from Scatterometer and Model",
+                 "Global Total (COPERNICUS-GLOBCURRENT), Ekman and Geostrophic currents at the Surface and 15m",
+                 "Multi Observation Global Ocean 3D Temperature Salinity Height Geostrophic Current and MLD",
+                 "Global Observed Ocean Physics 3D Quasi-Geostrophic Currents (OMEGA3D)",
+                 "SSS SMOS/SMAP L4 OI - LOPS-v2023",
+                 "Multi Observation Global Ocean Sea Surface Salinity and Sea Surface Density",
+                 "ECCO Ocean Mixed Layer Depth - Monthly Mean 0.5 Degree (Version 4 Release 4)",
+                 "GEBCO_2023 Grid (sub-ice topography/bathymetry)"]
--- update_script.py
+++ update_script.py
@@ -1,7 +1,10 @@
 from json_functions import read_json_to_dataframe, update_json_file
 from data_module_list import *
+import json
 
 filename = "ocean_data.json"
+with open(filename, "w") as file:
+        json.dump([], file,indent=4)
 for dataname in data_names:
     data = dataname.dataset
     update_json_file(filename,data)
