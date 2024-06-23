4192c7ce265699d5e4d591d462f14fa9e0455ead
Vanessa Madu <vm2218@ic.ac.uk>
2024-06-23 18:48:37 +0100

    download

---
+++ data_download.py
@@ -0,0 +1,170 @@
+import copernicusmarine
+import pandas as pd
+import glob
+import xarray as xr
+import os
+
+
+
+def generate_daily_timestamps(start_date, end_date):
+    return pd.date_range(start=start_date, end=end_date, freq='D').strftime('%Y-%m-%d 00:00:00').tolist()
+
+# subset request parameters
+options = ['short_geostrophic_velocities','short_wind','long_geostrophic_velocities']
+toggle = options[1]
+output_directory = "D:\PhD\ocean-datasets\copernicus-data"
+
+if toggle == 'short_geostrophic_velocities':
+    # SHORT TIME FRAME: 11/03/2022 - 22/06/2024
+    dataset_id = "cmems_obs-sl_glo_phy-ssh_nrt_allsat-l4-duacs-0.25deg_P1D"
+    variables = ["ugos","vgos","ugosa", "vgosa","err_ugosa","err_vgosa"]
+    # West NA
+    min_lon, max_lon = -83, -40 
+    min_lat, max_lat = 0, 60
+    start_date, end_date = "2022-03-11 12:00:00", "2024-06-22 23:00:00"
+    min_depth, max_depth = 0, 15
+    output_filename = "CMEMS_West_NA_currents_2022_2024.nc"
+
+    # Request subset
+    # Read credentials from environment variables
+    copernicusmarine.subset(
+        dataset_id=dataset_id,
+        variables=variables,
+        minimum_longitude=min_lon,
+        maximum_longitude=max_lon,
+        minimum_latitude=min_lat,
+        maximum_latitude=max_lat,
+        start_datetime=start_date,
+        end_datetime=end_date,
+        minimum_depth=min_depth,
+        maximum_depth=max_depth,
+        output_filename=output_filename,
+        output_directory=output_directory,
+    )
+
+elif toggle == 'short_wind':
+    # SHORT TIME FRAME: 11/03/2022 - 22/06/2024
+    dataset_id = "cmems_obs-wind_glo_phy_nrt_l4_0.125deg_PT1H"
+    variables = ["eastward_wind", "northward_wind","eastward_stress","northward_stress", "wind_curl","wind_divergence"]
+    # West NA
+    min_lon, max_lon = -83, -40 
+    min_lat, max_lat = 0, 60
+    start_date, end_date = "2022-03-11 12:00:00", "2024-06-22 23:00:00"
+    output_filename = "Full_CMEMS_West_NA_wind_2022_2024.nc"
+    timestamps = generate_daily_timestamps(start_date, end_date)
+
+    # Request subset for each timestamp
+    for timestamp in timestamps:
+        # Read credentials from environment variables
+        copernicusmarine.subset(
+            dataset_id=dataset_id,
+            variables=variables,
+            minimum_longitude=min_lon,
+            maximum_longitude=max_lon,
+            minimum_latitude=min_lat,
+            maximum_latitude=max_lat,
+            start_datetime=timestamp,
+            end_datetime=timestamp,
+            output_filename=f"CMEMS_West_NA_wind_{timestamp.replace(':', '').replace('-', '')}.nc",
+            output_directory=output_directory,
+        )
+    # List of all downloaded filesy
+    file_paths = sorted(glob.glob(f"{output_directory}/CMEMS_West_NA_wind_*.nc"))
+
+    # Open multiple files as a single dataset
+    datasets = [xr.open_dataset(file) for file in file_paths]
+    combined_dataset = xr.concat(datasets, dim='time')
+
+    # Save the combined dataset to a new NetCDF file
+    combined_dataset.to_netcdf(f"{output_filename}")
+    for file_path in file_paths:
+        os.remove(file_path)
+
+elif toggle == "long_geostrophic_velocities":
+    # LONGER TIME FRAME 01/01/1993 - 08/09/2023
+    dataset_id = "cmems_obs-sl_glo_phy-ssh_my[int]_allsat-l4-duacs-0.25deg_P1D"
+    variables = ["ugos","vgos","ugosa", "vgosa","err_ugosa","err_vgosa"]
+    # West NA
+    min_lon, max_lon = -83, -40 
+    min_lat, max_lat = 0, 60
+    start_date, end_date = "1993-01-01 00:00:00", "2023-09-08 23:00:00"
+    min_depth, max_depth = 0, 15
+    output_filename = "CMEMS_West_NA_currents_1993_2023.nc"
+
+    # Request subset
+    copernicusmarine.subset(
+        dataset_id=dataset_id,
+        variables=variables,
+        minimum_longitude=min_lon,
+        maximum_longitude=max_lon,
+        minimum_latitude=min_lat,
+        maximum_latitude=max_lat,
+        start_datetime=start_date,
+        end_datetime=end_date,
+        minimum_depth=min_depth,
+        maximum_depth=max_depth,
+        output_filename=output_filename,
+        output_directory=output_directory,
+    )
+
+elif toggle == 'long_wind':
+# LONGER TIME FRAME 01/01/1993 - 08/09/2023
+    dataset_id = "cmems_obs-wind_glo_phy_my_l4_0.125deg_PT1H"
+    variables = ["eastward_wind", "northward_wind","eastward_stress","northward_stress", "wind_curl","wind_divergence"]
+    # West NA
+    min_lon, max_lon = -83, -40 
+    min_lat, max_lat = 0, 60
+    start_date, end_date = "1994-06-01 00:00:00", "2023-09-08 23:00:00"
+    output_filename = "Full_CMEMS_West_NA_wind_1994_2023.nc"
+    timestamps = generate_daily_timestamps(start_date, end_date)
+
+    # Request subset for each timestamp
+    for timestamp in timestamps:
+        # Read credentials from environment variables
+        copernicusmarine.subset(
+            dataset_id=dataset_id,
+            variables=variables,
+            minimum_longitude=min_lon,
+            maximum_longitude=max_lon,
+            minimum_latitude=min_lat,
+            maximum_latitude=max_lat,
+            start_datetime=timestamp,
+            end_datetime=timestamp,
+            output_filename=f"CMEMS_West_NA_wind_{timestamp.replace(':', '').replace('-', '')}.nc",
+            output_directory=output_directory,
+        )
+    # List of all downloaded filesy
+    file_paths = sorted(glob.glob(f"{output_directory}/CMEMS_West_NA_wind_*.nc"))
+
+    # Open multiple files as a single dataset
+    datasets = [xr.open_dataset(file) for file in file_paths]
+    combined_dataset = xr.concat(datasets, dim='time')
+
+    # Save the combined dataset to a new NetCDF file
+    combined_dataset.to_netcdf(f"{output_filename}")
+    for file_path in file_paths:
+        os.remove(file_path)
+    
+elif toggle == 'long_sst':
+    # LONGER TIME FRAME 01/01/1993 - 08/09/2023
+    dataset_id = "SST-GLO-SST-L4-REP-OBSERVATIONS-010-011"
+    variables = ["analysed_sst","analysis_error"]
+    # West NA
+    min_lon, max_lon = -83, -40 
+    min_lat, max_lat = 0, 60
+    start_date, end_date = "1993-01-01 00:00:00", "2022-05-31 23:00:00"
+    output_filename = "CMEMS_West_NA_sst_1993_2022.nc"
+
+    # Request subset
+    copernicusmarine.subset(
+        dataset_id=dataset_id,
+        variables=variables,
+        minimum_longitude=min_lon,
+        maximum_longitude=max_lon,
+        minimum_latitude=min_lat,
+        maximum_latitude=max_lat,
+        start_datetime=start_date,
+        end_datetime=end_date,
+        output_filename=output_filename,
+        output_directory=output_directory,
+    )
