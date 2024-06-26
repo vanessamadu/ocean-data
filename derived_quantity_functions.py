7a3995f0e6a3f577a8723218a5dd6f0d644d9c53
Vanessa Madu <vm2218@ic.ac.uk>
2024-06-26 12:55:26 +0100

    iterate until convergence

--- derived_quantity_functions.py
+++ derived_quantity_functions.py
@@ -11,7 +11,22 @@ def cyclostrophic_correction(u, v, ug, vg, f):
     u_dot_grad_u_x = u * dv_dx + v * dv_dy
     u_dot_grad_u_y = u * du_dx + v * du_dy
     
-    correction_u = ug - (1 / f) * u_dot_grad_u_x
-    correction_v = vg + (1 / f) * u_dot_grad_u_y
+    corrected_u = ug - (1 / f) * u_dot_grad_u_x
+    corrected_v = vg + (1 / f) * u_dot_grad_u_y
     
-    return correction_u, correction_v
+    return corrected_u, corrected_v
+
+def iterate_until_convergence(u, v, ug, vg, f, tolerance=0.01, max_iterations=100):
+    for iteration in range(max_iterations):
+        u_new, v_new = cyclostrophic_correction(u, v, ug, vg, f)
+        
+        diff_u = np.max(np.abs(u_new - u))
+        diff_v = np.max(np.abs(v_new - v))
+        
+        if diff_u < tolerance and diff_v < tolerance:
+            print(f"Converged after {iteration} iterations")
+            break
+        
+        u, v = u_new, v_new
+    
+    return u, v
