ef1e3059beb4e2d1346c7a14d9b0f63d1d7a79b1
Vanessa Madu <vm2218@ic.ac.uk>
2024-06-27 13:35:00 +0100

    pause here: memory issues

--- sst_gradient_script.py
+++ sst_gradient_script.py
@@ -7,8 +7,8 @@ filename_short = "CMEMS_West_NA_sst_2022_2024.nc"
 output_filename_long = "derived_West_NA_sst_gradients_2012_2022.nc"
 output_filename_short = "derived_West_NA_sst_gradients_2022_2024.nc"
 # load the NetCDF file
-data_path = f"{output_directory}/{filename_long}"
+data_path = f"{output_directory}/{filename_short}"
 sst = xr.open_dataset(data_path).to_array()
 
 sst_gradient(sst,output_directory,output_filename_short)
-sst_gradient(sst,output_directory,output_filename_long)
+#sst_gradient(sst,output_directory,output_filename_long)
