e830274ba8df5de87b9906a1767ad8ce18ab5018
Vanessa Madu <vm2218@ic.ac.uk>
2024-07-14 16:21:10 +0100

    remove redundancy

--- sst_gradient_script.py
+++ sst_gradient_script.py
@@ -1,14 +1,15 @@
+import time
 from derived_quantity_functions import *
 
-output_directory = "D:\PhD\ocean-datasets\copernicus-data"
-filename_long = "CMEMS_West_NA_sst_2012_2022.nc"
-filename_short = "CMEMS_West_NA_sst_2022_2024.nc"
+start = time.time()
 
-output_filename_long = "derived_West_NA_sst_gradients_2012_2022.nc"
-output_filename_short = "derived_West_NA_sst_gradients_2022_2024.nc"
-# load the NetCDF file
-data_path = f"{output_directory}/{filename_short}"
-sst = xr.open_dataset(data_path).to_array()
+filename="CMEMS_West_NA_sst_2012_2022.nc"
+file_directory = "D:\PhD\ocean-datasets\copernicus-data"
+# Load the NetCDF file
+data_path = f"{file_directory}/{filename}"
+dataset = xr.open_dataset(data_path)
 
-sst_gradient(sst,output_directory,output_filename_short)
-#sst_gradient(sst,output_directory,output_filename_long)
+sst = dataset['analysed_sst'].isel(time=0)
+sst_gradient(sst,'./','sst_x_gradients_with_boundaries.nc')
+end = time.time()
+print(end-start)
