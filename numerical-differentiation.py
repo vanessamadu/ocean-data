d4958bca70f3cf388c23c4136f661de9ab0bfd16
Vanessa Madu <vm2218@ic.ac.uk>
2024-07-24 14:42:44 +0100

    as a function

--- numerical-differentiation.py
+++ numerical-differentiation.py
@@ -14,28 +14,33 @@ def stencil_mask(x,kernel_len):
             mask[ii] = stencil_is_valid
     return mask
 
-# len(x) >= 5
 x = np.array([1.0,2.0,4.0,5.0,7.0,np.nan,3.0,6.0])
-# define kernels
-kernel_stencil = np.array([1/12,-8/12,0, 8/12,-1/12])[::-1]
-kernel_central = np.array([-0.5,0,0.5])[::-1]
-kernel_onesided = np.array([-1.0,1.0])[::-1]
-# evaluate derivatives
-dx_stencil = stencil_mask(x,len(kernel_stencil))*convolve(x,kernel_stencil,normalize_kernel=False,nan_treatment='fill',boundary=None,
-                      mask=np.isnan(x))
-dx_central = stencil_mask(x,len(kernel_central))*convolve(x,kernel_central,normalize_kernel=False,nan_treatment='fill',boundary=None,
-                      mask=np.isnan(x))
-dx_right = convolve1d(x,kernel_onesided,mode="constant",cval=np.nan)
-dx_left = np.roll(dx_right,shift=1,axis=0)
 
-print(f"x: {x}")
-print(f"dx_stencil: {dx_stencil}")
-print(f"dx_central: {dx_central}")
-print(f"dx_left: {dx_left}")
-print(f"dx_right: {dx_right}")
+def diff1d(row):
+    ''' 
+    len(row) >= 5
+    '''
+    # define kernels
+    kernel_stencil = np.array([1/12,-8/12,0, 8/12,-1/12])[::-1]
+    kernel_central = np.array([-0.5,0,0.5])[::-1]
+    kernel_onesided = np.array([-1.0,1.0])[::-1]
+    # evaluate derivatives
+    dx_stencil = stencil_mask(x,len(kernel_stencil))*convolve(x,kernel_stencil,normalize_kernel=False,nan_treatment='fill',boundary=None,
+                        mask=np.isnan(x))
+    dx_central = stencil_mask(x,len(kernel_central))*convolve(x,kernel_central,normalize_kernel=False,nan_treatment='fill',boundary=None,
+                        mask=np.isnan(x))
+    dx_right = convolve1d(x,kernel_onesided,mode="constant",cval=np.nan)
+    dx_left = np.roll(dx_right,shift=1,axis=0)
 
-dx = np.where(np.isnan(dx_stencil),dx_central,dx_stencil)
-dx = np.where(np.isnan(dx),dx_left,dx)
-dx = np.where(np.isnan(dx),dx_right,dx)
+    # print(f"x: {x}")
+    # print(f"dx_stencil: {dx_stencil}")
+    # print(f"dx_central: {dx_central}")
+    # print(f"dx_left: {dx_left}")
+    # print(f"dx_right: {dx_right}")
 
-print(dx)
+    dx = np.where(np.isnan(dx_stencil),dx_central,dx_stencil)
+    dx = np.where(np.isnan(dx),dx_left,dx)
+    dx = np.where(np.isnan(dx),dx_right,dx)
+
+    return(dx)
+
