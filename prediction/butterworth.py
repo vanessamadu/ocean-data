7aeb284e291819c4fd5ef4a1e7332876e3ecc112
Vanessa Madu <vm2218@ic.ac.uk>
2024-07-30 11:54:45 +0100

    initial

---
+++ prediction/butterworth.py
@@ -0,0 +1,13 @@
+"""
+Script Name: butterworth.py
+Description: Applies a local Butterworth filter to drifter data based on drifter latitude
+
+Acknowledgment:
+This code is an iteration of the original code written by Dr Michael O'Malley to process data for the paper 
+"Multivariate Probabilistic Regression with Natural Gradient Boosting". Special thanks to Michael O'Malley for 
+providing the foundational code and methodologies that have been adapted and extended in this script.
+
+Author: Vanessa Madu
+Date: 30/07/2024
+Version: 1.0
+"""
